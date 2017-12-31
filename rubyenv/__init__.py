53fa39c1451657797c9d59771642761f79764999
jiho <search5@gmail.com>
2017-12-31 14:21:44 +0900

    Fixed to be compatible with Python 3

--- rubyenv/__init__.py
+++ rubyenv/__init__.py
@@ -1,2 +1,8 @@
-from _version import __version__
-from app import main
+import six
+
+if six.PY2:
+    from _version import __version__
+    from app import main
+elif six.PY3:
+    from ._version import __version__
+    from .app import main
